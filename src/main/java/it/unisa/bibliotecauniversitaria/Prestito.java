bf4a0ce6d579f6042732ed971901c2d544b19170
Rob <HugorHill@Rob.(none)>
2025-12-02 15:03:18 +0100

    Aggiunta classe Prestito (non ancora completa)

---
+++ src/main/java/it/unisa/bibliotecauniversitaria/Prestito.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.bibliotecauniversitaria;
+import java.util.*;
+import java.time.LocalDate;
+/**
+ *
+ * @author 177748
+ */
+public class Prestito {
+    private Set<Utente> utente;
+    private Set<Libro> libriInPrestito;
+    private LocalDate dataInizioPrestito;
+    private boolean ritardoPrestito;
+    
+    public Prestito(){
+        utente = new TreeSet<>();
+        libriInPrestito = new TreeSet<>();
+        
+    }
+    
+    
+    public void setDataInizioPrestito(LocalDate dataInizioPrestito){
+        this.dataInizioPrestito = dataInizioPrestito;
+    
+    }
+    
+    public LocalDate getDataInizioPrestito(){
+        return dataInizioPrestito;
+    }
+    
+    
+}
