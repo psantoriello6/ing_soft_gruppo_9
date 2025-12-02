21867a216f4d3f4d9968b856d4ac4ecda4fc8bd8
admin <admin@localhost>
2025-12-02 10:30:07 +0100

    adder modificata

--- src/main/java/it/unisa/bibliotecauniversitaria/Libro.java
+++ src/main/java/it/unisa/bibliotecauniversitaria/Libro.java
@@ -7,8 +7,74 @@ package it.unisa.bibliotecauniversitaria;
 
 /**
  *
- * @author pasqu
+ * @author chiara
  */
+
+
 public class Libro {
-    
+
+    private String titolo;
+    private int annoPubblicazione;
+    private String codice;
+    private int copieDisponibili;
+
+    // COSTRUTTORE
+
+    public Libro(String titolo, int annoPubblicazione, String codice, int copieDisponibili) {
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.codice = codice;
+        this.copieDisponibili = copieDisponibili;
+    }
+
+    // GETTER E SETTER
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public int getCopieDisponibili() {
+        return copieDisponibili;
+    }
+
+    public void setCopieDisponibili(int copieDisponibili) {
+        this.copieDisponibili = copieDisponibili;
+    }
+
+    // METODI 
+
+    public boolean hasCopieDisponibili() {
+        return copieDisponibili > 0;
+    }
+
+    public void decrementaCopie() {
+        if (copieDisponibili > 0)
+            copieDisponibili--;
+    }
+
+    public void incrementaCopie() {
+        copieDisponibili++;
+    }
+
 }
+
