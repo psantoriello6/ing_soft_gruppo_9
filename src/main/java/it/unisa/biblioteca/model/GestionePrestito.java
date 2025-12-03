010e735918b4a55dfee5f7cd4f3f15ef0d7f5f69
HugorHill <HugorHill@Rob.(none)>
2025-12-03 21:10:16 +0100

    Modifica documentazione Doxygen della classe Prestito + completamento classe GestionePrestito e documentazione Doxygen della classe GestionePrestito

--- src/main/java/it/unisa/biblioteca/model/GestionePrestito.java
+++ src/main/java/it/unisa/biblioteca/model/GestionePrestito.java
@@ -6,25 +6,68 @@
 package it.unisa.biblioteca.model;
 import java.util.*;
 /**
- *
- * @author 177748
+ * @file GestionePrestito.java
+ * @brief Questa classe contiene l'implementazione della classe GestionePrestito
+ * 
+ * La classe GestionePrestito prevede un attributo che rappresenta la lista dei prestiti, realizzata 
+ * mediante un'HashMap , un metodo per aggiungere un prestito e un metodo per registrare la restituzione di un prestito 
+ * 
+ * @date 3 dicembre 2025
  */
+
 public class GestionePrestito {
     private Map<Utente, LinkedList<Libro>> prestito;
     
+    /**
+     * @brief Costruttore della classe GestionePrestito
+     * 
+     * Il costruttore inizializza la collezione di prestiti utilizzando un'Hash Map
+     * 
+     */
+     
     public GestionePrestito(){
         prestito = new HashMap<>();
     
     }
     
-    public void aggiungiPrestito(Prestito prestito){
+    /**
+     * @brief Metodo che permette di registrare un prestito
+     * @param prestito Il prestito che si vuole registrare
+     * 
+     * @pre Il libro che si vuole prestare deve avere almento una copia disponibile
+     * @pre L'utente deve avere al massimo due libri in prestito contemporaneamente
+     * 
+     * @post Il prestito effettuato è stato registrato ed  inserito nella lista dei prestiti attivi
+     */
+    
+    public void inserisci(Prestito prestito){
     
     
     }
     
-    
-    public void restituisciPrestito(){
-        
+    /**
+     * @brief Metodo che permette di registrare la restituzione di un libro in prestito 
+     * @param prestito Il prestito di cui si vuole registrare la restituzione
+     * 
+     * @pre La lista dei prestiti attivi non deve essere vuota
+     * 
+     * @post La restituzione del prestito è stata registrata e il prestito è stato eliminato dalla lista di prestiti attivi
+     */
+    public void elimina(Prestito prestito){
+     
     }
     
+    /**
+     * @brief Metodo che permette di esportare su un file i dati relativi ad un presitio
+     * @param file Il nome del file su cui devono essere salvati i dati di un prestito
+     * 
+     * @pre I dati relativi ad un prestito sono stati inseriti nella collezione
+     * 
+     * @post I dati relativi ad un prestito sono stati esportati sul file
+     *  
+     */
+    public void salvaPrestiti(String file){
+    
+    
+    }
 }
