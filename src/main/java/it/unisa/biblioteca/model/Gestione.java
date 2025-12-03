893f440b4bdd9a6018954893819e02eff2b69188
admin <admin@localhost>
2025-12-03 18:31:42 +0100

    Inserimento commenti Doxygen per l'interfaccia Gestione

--- src/main/java/it/unisa/biblioteca/model/Gestione.java
+++ src/main/java/it/unisa/biblioteca/model/Gestione.java
@@ -6,9 +6,17 @@
 package it.unisa.biblioteca.model;
 
 /**
- *
- * @author admin
+ * @file Gestione.java
+ * 
+ * @brief Interfaccia generica utilizzata come modello dalle classi che gestiscono un tipo specifico di oggetto(Utente, Libro, Prestito)
+ * L'interfaccia è usata dalle classi GestioneUtente, GestioneLibro e GestionePrestito.
+ * Presenta i metodi inserisci, modifica ed elimina utilizzati per la gestione delle collezioni.
+ * 
+ * @param <T> il parametro che verrà gestito dalle classi che la implementano.
+ * 
+ * @date 3 Dicembre 2025
  */
+
 public interface Gestione<T> {
     
     public void inserisci(T elemento);
