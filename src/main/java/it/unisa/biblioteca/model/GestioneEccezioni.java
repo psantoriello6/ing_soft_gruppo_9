cff3e8928057baf3de052d8bac2f9229ef4db372
Rob <HugorHill@Rob.(none)>
2025-12-04 11:19:17 +0100

    Aggiunta classe GestioneEccezioni

---
+++ src/main/java/it/unisa/biblioteca/model/GestioneEccezioni.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.biblioteca.model;
+
+
+/**
+ * @file GestioneEccezioni.java
+ * @brief Questa classe contiene l'implementazione della classe GestioneEccezione
+ * 
+ * La classe permette, in caso di errori di inserimento dati verificatisi nelle varie classi del Model, di lanciare un'eccezione stampando un messaggio
+ * di errore appropriato.
+ * 
+ * @date 3 dicembre 2025
+ * 
+ */
+public class GestioneEccezioni extends Exception {
+    
+    /**
+     * @brief Costruttore della classe Gestione Eccezioni
+     *  
+     * Il costruttore della classe GestioneEccezioni prende come parametro e stampa una stringa che rappresenta un messaggio di errore
+     * 
+     * @param errorMessage Il messaggio di errore che si vuole stampare
+     */
+    public GestioneEccezioni(String errorMessage){
+        System.out.println(errorMessage);
+    
+    }
+    
+    
+}
