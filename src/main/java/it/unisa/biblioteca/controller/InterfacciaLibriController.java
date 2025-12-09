8fce7468dd05fea0d1f37efca5323daab1bcfd9f
Rob <HugorHill@Rob.(none)>
2025-12-09 22:30:24 +0100

    Inizio Implementazione InterfacciaLibriController

--- src/main/java/it/unisa/biblioteca/controller/InterfacciaLibriController.java
+++ src/main/java/it/unisa/biblioteca/controller/InterfacciaLibriController.java
@@ -5,6 +5,19 @@
  */
 package it.unisa.biblioteca.controller;
 
+import it.unisa.biblioteca.model.GestioneLibro;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import it.unisa.biblioteca.model.Libro;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
 /**
  * @file InterfacciaLibriController.java
  * @author Gruppo 9
@@ -15,5 +28,69 @@ package it.unisa.biblioteca.controller;
  * @date 03 Dicembre, 2025
  */
 public class InterfacciaLibriController {
+    
+    @FXML
+    private TableView<Libro> tabellaLibri;
+    @FXML
+    private TableColumn<Libro, String> colonnaTitoloLibri;
+    @FXML
+    private TableColumn<Libro, String> colonnaNomeLibri;
+    @FXML
+    private TableColumn<Libro, String> colonnaCognomeLibri;
+    @FXML
+    private TableColumn<Libro, Integer> colonnaAnnoLibri;
+    @FXML
+    private TableColumn<Libro, String> colonnaCodiceLibri;
+    @FXML
+    private TableColumn<Libro, Integer> colonnaCopieLibri;
+    
+    @FXML
+    private Button btnNuoviLibri;
+    
+    @FXML
+    private Button btnModificaLibri;
+    
+    @FXML 
+    private Button btnEliminaLibri;
+    
+    @FXML
+    private TextField tfLibri;
+    
+    @FXML
+    private MenuButton filterLibri;
+    
+    @FXML
+    private Button btnCercaLibri;
+    
+    //lista per costruire la TableView
+    private ObservableList<Libro> listaLibri;
+    
+    
+    public void inizializza(){
+        colonnaTitoloLibri.setCellValueFactory(new PropertyValueFactory<>("titolo"));
+        colonnaNomeLibri.setCellValueFactory(new PropertyValueFactory<>("nomeAutore"));
+        colonnaCognomeLibri.setCellValueFactory(new PropertyValueFactory<>("cognomeAutore"));
+        colonnaAnnoLibri.setCellValueFactory(new PropertyValueFactory<>("annoPubblicazione"));
+        colonnaCodiceLibri.setCellValueFactory(new PropertyValueFactory<>("codice"));
+        colonnaCopieLibri.setCellValueFactory(new PropertyValueFactory<>("copieDisponibili"));
+        
+        listaLibri = FXCollections.observableArrayList();
+        this.aggiornaTabella();
+    
+    }
+    
+    //metodo che aggiorna la lista osservabile
+    public void aggiornaTabella(){
+        listaLibri.clear();  //prima rimuovo tutto
+        listaLibri.addAll(GestioneLibro.getInstance().getSetLibro()); //inserisco nella lista osservabile tutti gli elementi del set di libri
+        if(listaLibri != null){
+            tabellaLibri.setItems(listaLibri);  //aggiungo la lista osservabile nella TableView
+        }
+    
+    }
+    
+    
+    
+    
     
 }
